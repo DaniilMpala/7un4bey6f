c905e6cba30b7eeef97880ca04e8b1f34412f468
Alexey <alex967268@gmail.com>
2021-07-21 23:10:32 +0300

    complete support-chat

--- src/Components/Support.jsx
+++ src/Components/Support.jsx
@@ -36,22 +36,68 @@ const Support = () => {
                                 </div>
                             </div>
 
+                            <div className="messages__item item my-message">
+                                <div className="item__body">
+                                    <div className="item__grid">
+
+                                        <div className="item__name">Danila</div>
+
+                                        <div className="item__msg">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis veniam cumque sit architecto perspiciatis quo placeat officia maxime hic, ex fuga laborum eveniet. Atque tenetur temporibus suscipit voluptatibus earum architecto!</div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="messages__item item">
+                                <div className="item__body">
+                                    <div className="item__grid">
+
+                                        <div className="item__name">Danila</div>
+
+                                        <div className="item__msg">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis veniam cumque sit architecto perspiciatis quo placeat officia maxime hic, ex fuga laborum eveniet. Atque tenetur temporibus suscipit voluptatibus earum architecto!</div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="messages__item item my-message">
+                                <div className="item__body">
+                                    <div className="item__grid">
+
+                                        <div className="item__name">Danila</div>
+
+                                        <div className="item__msg">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis veniam cumque sit architecto perspiciatis quo placeat officia maxime hic, ex fuga laborum eveniet. Atque tenetur temporibus suscipit voluptatibus earum architecto!</div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="messages__item item">
+                                <div className="item__body">
+                                    <div className="item__grid">
+
+                                        <div className="item__name">Danila</div>
+
+                                        <div className="item__msg">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis veniam cumque sit architecto perspiciatis quo placeat officia maxime hic, ex fuga laborum eveniet. Atque tenetur temporibus suscipit voluptatibus earum architecto!</div>
+                                    </div>
+                                </div>
+                            </div>
+
                             <div />
                         </div>
                     </div>
                 </div>
                 <div className="support-chat__send send">
-                    <textarea className="send__text main-input" cols="30" rows="4" placeholder="Ваше сообщение" type="text" name="send_text" />
-
-                    <input className="send__attach-photo" type="file" size="3" multiple accept="image/*" />
-                    <label className="send__attach-photo-icon" for="file">
+                    <input className="send__attach-photo" id="attach-img" name="file" type="file" size="3" multiple accept="image/*" />
+                    <label className="send__attach-photo-icon" for="attach-img">
                         <img src="img/attach_image.svg" alt="send" />
                     </label>
 
+                    <textarea className="send__text main-input" cols="30" rows="3" placeholder="Ваше сообщение" type="text" name="send_text" />
 
-                    <button className="send__btn main-btn">
-                        <img src="img/send_icon.svg" alt="send" />
-                    </button>
+                    <div className="send__btn">
+                        <button className="main-btn-v2">
+                            <img src="img/send_icon.svg" alt="send" />
+                        </button>
+                    </div>
+
                 </div>
             </div>
 
